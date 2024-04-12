fcd91b3cb75c2254581da7383e857bcd18b8bdc7
Luboš Matejčík <lma@panter.ch>
2024-04-12 21:05:41 +0200

    add surround plugin

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -313,4 +313,15 @@ return {
   { "mg979/vim-visual-multi" },
   { "andymass/vim-matchup" },
   { "github/copilot.vim" },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
 }
