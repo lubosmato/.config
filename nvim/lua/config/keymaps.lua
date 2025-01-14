1207ac7dc93da3d677bad58b2e4d9f77bd669aa4
Luboš Matejčík <lma@panter.ch>
2025-01-14 11:15:49 +0100

    feat(nvim): add fzf cword

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,3 +12,11 @@ vim.g.copilot_no_tab_map = true
 map("n", "<leader>fl", "<cmd>FzfLua resume<cr>", { noremap = true, silent = true, desc = "Resume FzfLua" })
 map("n", "<leader>fa", "<cmd>FzfLua<cr>", { noremap = true, silent = true, desc = "Find anything" })
 
+local fzf_cword = function()
+  local word = vim.fn.expand("<cword>")
+  require("fzf-lua").grep({ search = word })
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-g>", true, true, true), "n", false)
+end
+
+map("n", "<leader>*", fzf_cword, { noremap = true, silent = true, desc = "Grep word under cursor" })
+map("n", "<leader>#", fzf_cword, { noremap = true, silent = true, desc = "Grep word under cursor" })
