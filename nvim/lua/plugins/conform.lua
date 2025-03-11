b26d946279fca6c057a1d1e3247e8abfd4e2f5b5
lubosmato <lubos.matejcik@gmail.com>
2025-03-11 07:23:44 +0100

    chore(nvim): disable biome

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,31 +1,31 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = {
-      formatters = {
-        biome = {
-          command = "biome",
-          args = {
-            "check",
-            "--write",
-            "--stdin-file-path",
-            "$FILENAME",
-          },
-        },
-      },
-      formatters_by_ft = {
-        ["astro"] = { "biome" },
-        ["css"] = { "biome" },
-        ["graphql"] = { "biome" },
-        ["javascript"] = { "biome" },
-        ["javascriptreact"] = { "biome" },
-        ["json"] = { "biome" },
-        ["jsonc"] = { "biome" },
-        ["svelte"] = { "biome" },
-        ["typescript"] = { "biome" },
-        ["typescriptreact"] = { "biome" },
-        ["vue"] = { "biome" },
-      },
-    },
+    -- opts = {
+    --   formatters = {
+    --     biome = {
+    --       command = "biome",
+    --       args = {
+    --         "check",
+    --         "--write",
+    --         "--stdin-file-path",
+    --         "$FILENAME",
+    --       },
+    --     },
+    --   },
+    --   formatters_by_ft = {
+    --     ["astro"] = { "biome" },
+    --     ["css"] = { "biome" },
+    --     ["graphql"] = { "biome" },
+    --     ["javascript"] = { "biome" },
+    --     ["javascriptreact"] = { "biome" },
+    --     ["json"] = { "biome" },
+    --     ["jsonc"] = { "biome" },
+    --     ["svelte"] = { "biome" },
+    --     ["typescript"] = { "biome" },
+    --     ["typescriptreact"] = { "biome" },
+    --     ["vue"] = { "biome" },
+    --   },
+    -- },
   },
 }
