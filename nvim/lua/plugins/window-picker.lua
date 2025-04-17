891fc75e9303f108d298230396b66178fb082b6f
lubosmato <lubos.matejcik@gmail.com>
2025-04-17 07:52:49 +0200

    feat(nvim): add window picker

---
+++ nvim/lua/plugins/window-picker.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    config = {
+      highlights = {
+        enabled = true,
+        statusline = {
+          focused = {
+            fg = "#ededed",
+            bg = "#e35e4f",
+            bold = true,
+          },
+          unfocused = {
+            fg = "#ededed",
+            bg = "#44cc41",
+            bold = true,
+          },
+        },
+        winbar = {
+          focused = {
+            fg = "#ededed",
+            bg = "#e35e4f",
+            bold = true,
+          },
+          unfocused = {
+            fg = "#ededed",
+            bg = "#44cc41",
+            bold = true,
+          },
+        },
+      },
+    },
+  },
+}
