11db6f76e9ec381083d5913a28dcb03f0c7e2559
lubosmato <lubos.matejcik@gmail.com>
2025-06-07 07:37:16 +0800

    chore(nvim): add aider config

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -7,12 +7,17 @@ return {
       auto_manage_context = true, -- automatically manage buffer context
       default_bindings = true, -- use default <leader>A keybindings
       debug = false, -- enable debug logging
+      -- how to configure to use different model, AI?
     },
     keys = {
-      { "<leader>aa", ":AiderOpen --no-auto-commits --code-theme=one-dark<CR>", desc = "Open aider" },
+      {
+        "<leader>aa",
+        ":AiderOpen --config ~/.config/.aider.conf.yml<CR>",
+        desc = "Open aider",
+      },
       {
         "<leader>aw",
-        ":AiderOpen --no-auto-commits --code-theme=one-dark --watch-files<CR>",
+        ":AiderOpen --config ~/.config/.aider.conf.yml --watch-files<CR>",
         desc = "Watch files with aider",
       },
       { "<leader>am", ":AiderAddModifiedFiles<CR>", desc = "Add modified files to aider" },
